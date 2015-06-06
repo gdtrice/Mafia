a0707981b4c4793647cc6ee247492bb8913ffa0b
Garland Trice <GDTrice@hotmail.com>
2015-06-06 14:45:56 -0700

    add game room polling

--- static/js/app/views/game_room.js
+++ static/js/app/views/game_room.js
@@ -1,18 +1,42 @@
 define([
     "react",
-    "views/player_list"
+    "views/player_list",
+    "models/game",
 ],
 function (
     React,
-    PlayerList
+    PlayerList,
+    GameModel
     ) {
     return React.createClass({
+        POLL_INTERVAL: 3000, // 3 seconds
+
+        getInitialState: function() {
+            return {game: this.props.game};
+        },
+
+        componentDidMount: function() {
+            setInterval(this.loadPlayers, this.POLL_INTERVAL);
+        },
+
+        loadPlayers: function() {
+            var self = this;
+            this.props.game.fetch({
+                success: function(resp, status) {
+                    self.setState({game: resp});
+                },
+                error: function(resp, status) {
+                    console.log(status);
+                }
+            });
+        },
+
         render: function() {
             return (
                 <div className="game-room">
-                    <div>Game Room: { this.props.game.id }</div>
+                    <div>Game Room: { this.state.game.id }</div>
                     <div>Current Players:</div>
-                    <PlayerList players={ this.props.game.get("players") } />       
+                    <PlayerList players={ this.state.game.get("players") } />
                 </div>
             );
         }
