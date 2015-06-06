d7a6db34ef534bc7331b9c5db8bb49c4b0694d12
Garland Trice <GDTrice@hotmail.com>
2015-06-06 15:57:50 -0700

    move username/picture stuff to config view

--- static/js/app/models/game_config.js
+++ static/js/app/models/game_config.js
@@ -11,23 +11,7 @@ function (
     Games
     ) {
 
-    // Mocking some the service for now...
     return Backbone.Model.extend({
-        MAX_GAMES: 100,
-        IMAGES: [
-            "https://pbs.twimg.com/profile_images/450834967166070784/QeQgc7ZF_400x400.jpeg",
-            "https://pbs.twimg.com/profile_images/585565077207678977/N_eNSBXi_400x400.jpg",
-            "https://pbs.twimg.com/profile_images/566325718330511360/ggWoZsVl_400x400.jpeg",
-            "https://pbs.twimg.com/profile_images/378800000189727654/ab49635c2ea63d32c066c899e21fa8ce_400x400.jpeg",
-            "https://pbs.twimg.com/profile_images/581580930520727552/hE2LErp8_400x400.jpg",
-            "https://pbs.twimg.com/profile_images/543489020508921856/hM1FwyAO_reasonably_small.jpeg",
-            "http://weknowmemes.com/wp-content/uploads/2012/06/i-feel-it.jpg",
-            "http://static.comicvine.com/uploads/original/5/50639/3377081-walter+white.jpg",
-            "http://www.adweek.com/files/blogs/old-spice-terry-crews-brain-explodes.jpg",
-            "http://s3.jspenguin.org/dodgeball_fran.jpeg",
-            "http://i0.kym-cdn.com/photos/images/facebook/000/017/382/1250991947631.jpg",
-        ],
-
         initialize: function() {
             this.gameCount = this.MAX_GAMES;
             this.usedGameCodes = [];
@@ -35,13 +19,11 @@ function (
             this.games = new Games();
         },
 
-        createGame: function(username, options) {
+        createGame: function(username, picture, options) {
             /*this.gameCount--;
             this.usedGameCodes.push(this.gameCount);
             return this.gameCount;*/
             
-            var username = "testuser" + this._randomness();
-            var picture = _.sample(this.IMAGES); 
             // TODO: This should be in Game model...
             var newGame = {
                 "createDate": "test",
@@ -69,17 +51,8 @@ function (
         },
 
         joinGame: function(gameCode, username, picture) {
-            // TODO: need a better way to query for the game
-            var username = "testuser" + this._randomness();
-            var picture = _.sample(this.IMAGES); 
-            var game = this.games.joinGame(gameCode, username, picture);
-            return game;
+            return this.games.joinGame(gameCode, username, picture);
         },
 
-        _randomness: function() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-          .toString(16)
-          .substring(1);
-        }
     });
 });
--- static/js/app/views/config.js
+++ static/js/app/views/config.js
@@ -9,14 +9,33 @@ function (
     GameConfig
     ) {
     return React.createClass({
+        IMAGES: [
+            "https://pbs.twimg.com/profile_images/450834967166070784/QeQgc7ZF_400x400.jpeg",
+            "https://pbs.twimg.com/profile_images/585565077207678977/N_eNSBXi_400x400.jpg",
+            "https://pbs.twimg.com/profile_images/566325718330511360/ggWoZsVl_400x400.jpeg",
+            "https://pbs.twimg.com/profile_images/378800000189727654/ab49635c2ea63d32c066c899e21fa8ce_400x400.jpeg",
+            "https://pbs.twimg.com/profile_images/581580930520727552/hE2LErp8_400x400.jpg",
+            "https://pbs.twimg.com/profile_images/543489020508921856/hM1FwyAO_reasonably_small.jpeg",
+            "http://weknowmemes.com/wp-content/uploads/2012/06/i-feel-it.jpg",
+            "http://static.comicvine.com/uploads/original/5/50639/3377081-walter+white.jpg",
+            "http://www.adweek.com/files/blogs/old-spice-terry-crews-brain-explodes.jpg",
+            "http://s3.jspenguin.org/dodgeball_fran.jpeg",
+            "http://i0.kym-cdn.com/photos/images/facebook/000/017/382/1250991947631.jpg",
+        ],
+
         componentDidMount: function() {
             this.gameConfig = new GameConfig();
         },
 
         joinGame: function(e) {
             e.preventDefault();
+
+            // creating a fake username for now
+            var username = "joinedUser" + this._randomness();
+            var picture = _.sample(this.IMAGES); 
             var gameCode = React.findDOMNode(this.refs.gameCode).value.trim();
-            var joinedGame = this.gameConfig.joinGame(gameCode, "newUserHere2");
+            var joinedGame = this.gameConfig.joinGame(gameCode, username, picture);
+
             if(_.isEmpty(joinedGame)){
                 // error callback
             };
@@ -27,9 +46,10 @@ function (
             e.preventDefault();
 
             // creating a fake username for now
-            var username = "firstUser";
+            var username = "creatorUser" + this._randomness();
+            var picture = _.sample(this.IMAGES); 
             var self = this;
-            this.gameConfig.createGame(username, {
+            this.gameConfig.createGame(username, picture, {
                                        success: function(game) {
                                            self.props.onGameCodeRetrieved(game);
                                        },
@@ -51,6 +71,12 @@ function (
                     </form>
                 </div>
             );
+        },
+
+        _randomness: function() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
         }
     });
 });
