e562c5d1735e49b76610f9b31d2d63898db10ed6
Garland Trice <GDTrice@hotmail.com>
2015-06-12 02:18:07 -0700

    adding socket handlers for doc and detective

--- static/js/app/views/doctor.js
+++ static/js/app/views/doctor.js
@@ -1,13 +1,51 @@
 define([
     "react",
-    "underscore"
+    "underscore",
+    "socket.io",
+    "views/player_list"
 ],
 function (
     React,
-    _
+    _,
+    io,
+    PlayerListView
     ) {
     return React.createClass({
+        componentDidMount: function() {
+            this.socket = io();
+            this.socket.on('night_action', this._renderNightAction);
+            // TODO: change kill event for doc
+            this.socket.on('kill_registered', this._renderNightWait);
+        },
+
+        getInitialState: function() {
+            return {nightAction: false,
+                    nightWait: false,
+                    day: false};
+        },
+
+        _renderNightAction: function(data) {
+            this.setState({nightAction: true,
+                           nightWait: false});
+        },
+
+        _renderNightWait: function(data) {
+            this.setState({nightAction: false,
+                           nightWait: true});
+        },
         render: function() {
+            if(this.state.nightAction === true) {
+                return (
+                    <div>
+                        <div> Its night time player...save someones life! </div>
+                        <PlayerListView players={ this.props.game.get('players') } />
+                    </div>
+                );
+            } else if (this.state.nightWait === true) {
+                return (
+                        <div> Night time...Please wait! </div>
+                );
+            }
             var tempStyle = {
                 width: "100px",
                 height: "100px"
